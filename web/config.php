8bfbc7516e917e7a7fea1cdb880f479b9f801300
kolevbm <kolev.bm@gmail.com>
2025-06-29 19:48:33 +0300

    php config file remove env varsafga

--- web/config.php
+++ web/config.php
@@ -1,12 +1,13 @@
-<?php
+<!-- <?php
    $database = "bulgaria";
    $user = "web_user";
    $password  = "Password1";
    $host = "db";
-?>
-<!-- <?php
+?> -->
+
+<?php
    $database = getenv('DATABASE');
    $user = getenv('DB_USER');
    $password  = getenv('DB_PASSWORD');
    $host = getenv('DB_HOST');
-?> -->
+?>
