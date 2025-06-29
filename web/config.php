262f6627fcce9728548a56eb9dfba3ee81b8821d
kolevbm <kolev.bm@gmail.com>
2025-06-29 19:43:26 +0300

    php config file remove env vars

--- web/config.php
+++ web/config.php
@@ -1,6 +1,12 @@
 <?php
+   $database = "bulgaria";
+   $user = "web_user";
+   $password  = "Password1";
+   $host = "db";
+?>
+<!-- <?php
    $database = getenv('DATABASE');
    $user = getenv('DB_USER');
    $password  = getenv('DB_PASSWORD');
    $host = getenv('DB_HOST');
-?>
+?> -->
