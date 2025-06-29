4645e8daa07d5d1856cb07dcd15502b325b4f2a1
kolevbm <kolev.bm@gmail.com>
2025-06-29 19:59:23 +0300

    php config file remove env varsafgsdfasda

--- web/config.php
+++ web/config.php
@@ -1,13 +1,6 @@
 <?php
-   $database = "bulgaria";
-   $user = "web_user";
-   $password  = "Password1";
-   $host = "db";
-?>
-
-<!-- <?php
    $database = getenv('DATABASE');
    $user = getenv('DB_USER');
    $password  = getenv('DB_PASSWORD');
    $host = getenv('DB_HOST');
-?> -->
+?>
